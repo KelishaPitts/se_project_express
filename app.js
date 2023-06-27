a2502f1896b0ee8f57e37f9f793bb4b300adc6d1
Kelisha Pitts <kelishapitts@protonmail.com>
2023-06-26 21:43:22 -0400

    end points

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+
 const { PORT = 3001 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db"),
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db'),
   (r) => {
-    console.log("connect to database");
+    console.log('connect to database');
   },
-  (err) => console.log("database error", err);
+  (err) => console.log('database error', err);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
+    _id: '5d8b8592978f8bd833ca8133',
   };
   next();
   module.exports.createClothingItem = (req, res) => {
@@ -19,11 +20,11 @@ app.use((req, res, next) => {
   };
 });
 
-const routes = require("./routes");
+const routes = require('./routes');
 app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-  console.log("It's alive!!!");
+  console.log('It is alive!!!');
 });
