62cffcb3b7a74349fb63435eeb8fabeaa99115dd
Kelisha Pitts <kelishapitts@protonmail.com>
2023-06-26 22:49:27 -0400

    Update

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3001 } = process.env;
 const app = express();
 
-function connectToDatabase() {
-  mongoose
-    .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
     .then(() => {
-      console.log("Connected to database");
+      console.log('Connected to the database');
     })
-    .catch((err) => {
-      console.log("Database error", err);
+    .catch((error) => {
+      console.log('Database connection error:', error);
     });
-}
-connectToDatabase();
-app.use((req, next) => {
+
+app.use((req, res, next) => {
+  console.log(res)
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
+    _id: '5d8b8592978f8bd833ca8133',
   };
   next();
-  module.exports.createClothingItem = (req) => {
+  module.exports.createClothingItem = (req, res) => {
     console.log(req.user._id);
+    console.log(res)
   };
 });
 
-const routes = require("./routes");
+const routes = require('./routes');
+
 app.use(express.json());
 app.use(routes);
-
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-  console.log("It is alive!!!");
+  console.log('It is alive!!!');
 });
