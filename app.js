edbfab2e2b803be9acd447583a9ead9e6aeba898
Kelisha Pitts <kelishapitts@protonmail.com>
2023-06-26 22:57:59 -0400

    Update

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const mongoose = require('mongoose');
 const { PORT = 3001 } = process.env;
 const app = express();
 
-  mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
-    .then(() => {
-      console.log('Connected to the database');
-    })
-    .catch((error) => {
-      console.log('Database connection error:', error);
-    });
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.log('Database connection error:', error);
+  });
 
 app.use((req, res, next) => {
-  console.log(res)
+  console.log(res);
   req.user = {
     _id: '5d8b8592978f8bd833ca8133',
   };
   next();
-  module.exports.createClothingItem = (req, res) => {
+  module.exports.createClothingItem = () => {
     console.log(req.user._id);
     console.log(res)
   };
