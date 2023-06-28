1f1ffc4aca6ca3ff00ad2b889992da9ae22fbf35
Kelisha Pitts <kelishapitts@protonmail.com>
2023-06-28 00:40:49 -0400

    Ready for Review

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { limiter } = require("./utils/rateLimit");
 const helmet = require("helmet");
+const { limiter } = require("./utils/rateLimit");
 
 const { PORT = 3001 } = process.env;
 const app = express();
--- utils/rateLimit.js
+++ utils/rateLimit.js
@@ -1,4 +1,5 @@
 const rateLimit = require("express-rate-limit");
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 50,
